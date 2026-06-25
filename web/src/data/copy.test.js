5fb5fbf532fd13671c423247fbc23d80222410fe
dasibom-dev <ahn1st2024@gmail.com>
2026-06-25 11:15:20 +0900

    feat(web): add bilingual copy data

---
+++ web/src/data/copy.test.js
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import { copy } from './copy'
+
+describe('copy', () => {
+  it('ko와 en이 동일한 최상위 키를 가진다', () => {
+    expect(Object.keys(copy.ko).sort()).toEqual(Object.keys(copy.en).sort())
+  })
+  it('온보딩 타이틀이 한국어 카피를 가진다', () => {
+    expect(copy.ko.onbTitle).toContain('다시 봄')
+  })
+  it('영어 온보딩 타이틀이 존재한다', () => {
+    expect(copy.en.onbTitle.length).toBeGreaterThan(0)
+  })
+})
